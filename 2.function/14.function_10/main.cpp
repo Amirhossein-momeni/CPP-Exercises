f7597774a02c1e96c10cae31c37266014f9827d3
Amirhossein-momeni <amir.mo.80.2001@gmail.com>
2024-03-14 11:31:29 +0330

    update(main.cpp): putting up comment

--- 2.function/14.function_10/main.cpp
+++ 2.function/14.function_10/main.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
-int add (int, int);
+int add (int, int);//declaration
 
 int main(){
-    std::cout << "the sum of 3 and 4 is : "<<add(3, 4) << '\n';
-    return 0;
+    std::cout << "the sum of 3 and 4 is : "<< add( 3, 4 ) << '\n';    return 0;
 }
 
 
+//To run the program in the terminal, first write:   g++ main.cpp add.cpp       and then:     ./a.exe
